4415ad71e32079d33819383f628031b117f5b779
nemirof <nemirof@gmail.com>
2018-02-21 14:17:43 +0100

    Pruebassss

--- src/main/java/com/softtek/rest/IncidenciasCompletasController.java
+++ src/main/java/com/softtek/rest/IncidenciasCompletasController.java
@@ -3,6 +3,7 @@ package com.softtek.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.softtek.incidencias.IncidenciaCompleta;
+import com.softtek.incidencias.Incidencias;
 import com.softtek.incidencias.IncidenciasCompletasRepository;
 
 @RestController
@@ -23,6 +25,11 @@ public class IncidenciasCompletasController {
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public List<IncidenciaCompleta> getIncidencias(){
 		return incidenciasCompletasRepository.findAll();
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public void addIncidencia(@RequestBody IncidenciaCompleta incidencia){
+		incidenciasCompletasRepository.insert(incidencia);
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.DELETE)
